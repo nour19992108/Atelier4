fc592a50ad45de1586d91029b40ff1666c4489b3
Nourredinne <noureddine.boussekine@monboreal.ca>
2026-03-29 19:02:33 -0400

    Creation de la classe Employe

---
+++ Atelier4_POO.py
@@ -0,0 +1,6 @@
+class Employe:
+    def __init__(self,num_permis,nom,prenom):
+        self.num_permis=num_permis
+        self.nom=nom
+        self.prenom=prenom
+        self.voitureService =  None
